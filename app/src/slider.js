04e094ee4932bf3784a9369d547e7a87294ca00d
admin <admin@bizon.help>
2021-01-04 20:37:53 +0300

    init

---
+++ app/src/slider.js
@@ -0,0 +1,30 @@
+
+/**
+* Инициализация
+*
+* @copyright		Copyright (c) bizon.help
+* @author			roman 
+* @version			v.1.0 (04/01/2021)
+* @param			{object} options - параметры
+* @param			{string} options.selectorWrapper - селектор слоя со слайдами
+* @param			{string} options.selectorButtonLeft - селектор кнопки влево
+* @param			{string} options.selectorButtonRight - селектор кнопки вправо
+*/
+function sliderInit (options) {
+	// сохраняе селектор слоя
+	// скрываем все слайды
+	// показываем 1й слайд
+	// сохраняем показанный слайд
+}
+
+function sliderShiftLeft (options) {
+	// скрывает текущий слайд
+	// показывает предыдущий слайд (-1)
+	// декрементируем текущий слайд
+}
+
+function sliderShiftRight (options) {
+	// скрывает текущий слайд
+	// показывает последующий слайд (+1)
+	// инкрементируем текущий слайд
+}
